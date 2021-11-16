ef497891efbd10e7986eb0220a499f7d1a936e40
HildaAdelya <94032940+HildaAdelya@users.noreply.github.com>
2021-11-16 16:59:58 +0700

    Add files via upload

---
+++ STM32_INFRARED_LED_RFID_SERVO_LCD_BUZZER/STM32_INFRARED_LED_RFID_SERVO_LCD_BUZZER.ino
@@ -0,0 +1,125 @@
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
+
+//INISIALISASI
+
+int pinIR=PA4;
+int Merah=PA1;
+int Kuning=PA2;
+int Hijau=PA3;
+int outServo=PA0;
+int pinBuzzer=PB5;
+
+#define SS_PIN PB1
+#define RST_PIN PB0
+
+MFRC522 mfrc522(SS_PIN,RST_PIN);
+LiquidCrystal_I2C lcd(0x27, 16,2);
+Servo myservo;
+
+String uidString;
+int kunci=0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinIR,INPUT);
+  pinMode(Merah,OUTPUT); 
+  pinMode(Kuning,OUTPUT);
+  pinMode(Hijau,OUTPUT);
+  pinMode(pinBuzzer,OUTPUT);
+  lcd.begin();
+  
+  lcd.setCursor(1,0); lcd.print("SELAMAT DATANG");
+  lcd.setCursor(0,1); lcd.print("TEMPELKAN KARTU!");
+  SPI.begin();
+  mfrc522.PCD_Init();
+  myservo.attach(outServo);
+
+  bunyiBuzzeroke();
+}
+
+void readRFID(){
+  mfrc522.PICC_ReadCardSerial();
+
+  uidString = String(mfrc522.uid.uidByte[0],HEX) + "-" +
+              String(mfrc522.uid.uidByte[1],HEX) + "-" +
+              String(mfrc522.uid.uidByte[2],HEX) + "-" +
+              String(mfrc522.uid.uidByte[3],HEX) ;
+              uidString.toUpperCase();
+}
+
+void bunyiBuzzeroke(){
+  digitalWrite(pinBuzzer,HIGH);
+  delay(100);
+  digitalWrite(pinBuzzer,LOW);
+  delay(100);
+  digitalWrite(pinBuzzer,HIGH);
+  delay(100);
+  digitalWrite(pinBuzzer,LOW);
+  delay(100);
+}
+
+void bunyiBuzzergagal(){
+  digitalWrite(pinBuzzer,HIGH);
+  delay(1000);
+  digitalWrite(pinBuzzer,LOW);
+  delay(1000);
+}
+
+void loop() {
+  int bacaIR=digitalRead(pinIR);
+
+  if (mfrc522.PICC_IsNewCardPresent() and kunci==0){
+  readRFID();
+  Serial.print("Tag UID: ");
+  Serial.println(uidString);
+  if(uidString=="4A-51-6E-AE"){
+  lcd.clear();
+  lcd.setCursor(4,0); lcd.print("BERHASIL");
+  bunyiBuzzeroke();
+  digitalWrite(Merah,LOW);
+  digitalWrite(Kuning,LOW);
+  digitalWrite(Hijau,HIGH);
+  kunci=1;
+  delay(100);
+  lcd.setCursor(1,1); lcd.print("SILAHKAN MASUK");
+  myservo.write(90);
+  delay(500);
+  myservo.write(0);
+  delay(1000);
+  }else{
+  lcd.clear();
+  lcd.setCursor(5,0); lcd.print("GAGAL");
+  lcd.setCursor(0,1); lcd.print("BELUM TERDAFTAR");
+  delay(1000);
+  bunyiBuzzergagal();
+  digitalWrite(Merah,HIGH);
+  digitalWrite(Kuning,LOW);
+  digitalWrite(Hijau,LOW);  
+  myservo.write(90);
+  lcd.clear();
+  lcd.setCursor(1,0); lcd.print("SELAMAT DATANG");
+  lcd.setCursor(0,1); lcd.print("TEMPELKAN KARTU!");
+  }
+}
+
+  if (bacaIR==0 and kunci==1){
+  kunci=0;
+  delay(200);
+  myservo.write(90);
+  lcd.clear();
+  lcd.setCursor(1,0); lcd.print("PALANG DITUTUP");
+  delay(1000);
+  digitalWrite(Kuning,HIGH);
+  delay(400);
+  digitalWrite(Kuning,LOW);
+  digitalWrite(Merah,HIGH);
+  digitalWrite(Hijau,LOW);
+  lcd.clear();
+  lcd.setCursor(1,0); lcd.print("SELAMAT DATANG");
+  lcd.setCursor(0,1); lcd.print("TEMPELKAN KARTU!");
+ }
+}
